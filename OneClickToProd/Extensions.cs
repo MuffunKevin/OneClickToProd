3f9f5fb17080237aeb6b15e711841e0501c1298f
MuffunKevin <muffunkevin@gmail.com>
2015-07-07 21:12:26 -0400

    Add extension
    Add the extension of null or empty

---
+++ OneClickToProd/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneClickToProd
+{
+    public static class Extensions
+    {
+        public static bool isNullOrEmpty(this string item)
+        {
+            return String.IsNullOrEmpty(item);
+        }
+    }
+}
